0ef3daf0ff76bf736b3d7352296e9c7d5cb65c35
rohini-19785 <58015099+rohini-19785@users.noreply.github.com>
2020-04-24 17:02:27 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/youtoocan/YouTooCanApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.youtoocan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTooCanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouTooCanApplication.class, args);
+	}
+
+}
