b48d98a2946ad3fb0f21827219d9157e5ef86942
rohini-19785 <58015099+rohini-19785@users.noreply.github.com>
2020-04-27 17:31:53 -0500

    landing page added!

--- src/main/java/org/launchcode/youtoocan/controllers/HomeController.java
+++ src/main/java/org/launchcode/youtoocan/controllers/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+
 public class HomeController {
 
     @RequestMapping("")
@@ -13,6 +14,13 @@ public class HomeController {
 
 
         return "index";
+    }
+
+    @RequestMapping("wanttohelp")
+    public String wantToHelpPage(Model model) {
+
+
+        return "wanttohelp";
     }
 
 }
